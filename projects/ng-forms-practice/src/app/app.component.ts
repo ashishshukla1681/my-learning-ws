afebddf6faebd95d72b047696ade2a9e552c3556
Ashish Kr Shukla <SHUKLAA1@legal.regn.net>
2023-06-15 23:16:31 -0400

    Practice reactive form controls

--- projects/ng-forms-practice/src/app/app.component.ts
+++ projects/ng-forms-practice/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'ng-forms-practice';
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signupForm: FormGroup | undefined;
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      'gender': new FormControl('male')
+    });
+
+    //console.log(this.signupForm);
+
+    this.signupForm.get
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+
 }
